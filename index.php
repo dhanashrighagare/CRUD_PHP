609c133269ac3998a6a2ea30774a76351168d32b
dhanashrighagare <dhanashrighagare12@gmail.com>
2024-06-07 19:55:33 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <style>
+        .container {
+            margin-top: 50px;
+        }
+        .btn {
+            margin: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2>Student Management System</h2>
+        <div class="btn-group">
+            <a href="create-student.php" class="btn btn-primary">Create New Student</a>
+            <a href="view-student.php" class="btn btn-info">View Students</a>
+        </div>
+    </div>
+</body>
+</html>
